6f286d0714222281953170f9817e964b5b884dc5
Yevhenii Bieliaiev <yevheniibieliaiev@gmail.com>
2023-06-20 13:10:12 +0300

    da: changed path to db

--- packages/backend/src/repositories/index.ts
+++ packages/backend/src/repositories/index.ts
@@ -1,12 +1,10 @@
-import path from 'path';
 import type { InitRepositories } from '@interfaces';
 import { ShopsRepository } from './shops';
 import { AuthRepository } from './auth';
 import { OrdersRepository } from './orders';
 
 export const initRepositories = (): InitRepositories => {
-  const serverPath = path.dirname(process.argv[1]);
-  const pathToDb = path.join(serverPath, 'data-base');
+  const pathToDb = '../data-base';
 
   return {
     shopsRepository: new ShopsRepository({ pathToDb }),
